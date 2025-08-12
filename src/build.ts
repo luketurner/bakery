e6096f325fd8a0c0c9f316f9903d4d703cda0c97
Luke Turner <github@luketurner.org>
2025-08-11 21:12:05 -0700

    update build script to remove weave-specific stuff

--- src/build.ts
+++ src/build.ts
@@ -4,6 +4,7 @@
 import { $ } from "bun";
 import { mkdir } from "fs/promises";
 import { rm } from "fs/promises";
+import { getPackageJson } from "./util";
 
 const targets = [
   "bun-linux-x64",
@@ -17,11 +18,11 @@ const targets = [
 
 async function buildTarget(
   target: string,
-  { outdir, skipCompress }: { outdir: string; skipCompress: boolean },
+  { outdir, skipCompress, entrypoint, name }: { outdir: string; skipCompress: boolean; entrypoint: string; name: string; },
 ) {
-  const filename = `weave-${target.replace("bun-", "")}`;
+  const filename = `${name}-${target.replace("bun-", "")}`;
   console.log(`Building ${target} into ${outdir}/${filename}...`);
-  await $`bun build --define RELEASE=true --compile --minify --sourcemap src/index.tsx --outfile ${outdir}/${filename} --target ${target}`.env(
+  await $`bun build --define RELEASE=true --compile --minify --sourcemap ${entrypoint} --outfile ${outdir}/${filename} --target ${target}`.env(
     {
       NODE_ENV: "production",
     },
@@ -31,11 +32,11 @@ async function buildTarget(
     await $`chmod u+r ${filename}.exe`.cwd(outdir);
     // since zip doesn't have a --transform equivalent, just use a subdirectory and rename
     await mkdir(`${outdir}/${filename}`);
-    await $`cp ${filename}.exe ${filename}/weave.exe`.cwd(outdir);
-    await $`zip -9 ../${filename}.zip weave.exe`.cwd(`${outdir}/${filename}`);
+    await $`cp ${filename}.exe ${filename}/${name}.exe`.cwd(outdir);
+    await $`zip -9 ../${filename}.zip ${name}.exe`.cwd(`${outdir}/${filename}`);
     await rm(`${outdir}/${filename}`, { recursive: true });
   } else {
-    await $`tar -czf ${filename}.tar.gz --transform='s/${filename}/weave/' ${filename}`.cwd(
+    await $`tar -czf ${filename}.tar.gz --transform='s/${filename}/${name}/' ${filename}`.cwd(
       outdir,
     );
   }
@@ -52,17 +53,31 @@ export async function build({
   buildPackage: boolean;
   skipCompress: boolean;
 }): Promise<void> {
+
+  const packageJson = getPackageJson();
+
+  const entrypoint: string | undefined = getPackageJson().module;
+  if (!entrypoint) {
+    throw new Error('Must specify module in package.json');
+  }
+
+  const name: string | undefined = getPackageJson().name;
+  if (!name) {
+    throw new Error('Must specify name in package.json');
+  }
+
+
   await rm(outdir, { recursive: true, force: true });
 
   if (target && targets.includes(target)) {
-    await buildTarget(target, { outdir, skipCompress });
+    await buildTarget(target, { outdir, skipCompress, entrypoint });
   } else {
     await Promise.all(
-      targets.map((target) => buildTarget(target, { outdir, skipCompress })),
+      targets.map((target) => buildTarget(target, { outdir, skipCompress, entrypoint, name })),
     );
   }
 
   if (buildPackage) {
-    await $`bun pm pack --filename ${outdir}/weave-pkg.tar.gz`;
+    await $`bun pm pack --filename ${outdir}/${name}-pkg.tar.gz`;
   }
 }
