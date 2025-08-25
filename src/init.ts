b7a08566f944654ad76740597e1566348808efed
Luke Turner <github@luketurner.org>
2025-08-25 13:03:11 -0700

    update init to use npm package instead of git repo

--- src/init.ts
+++ src/init.ts
@@ -43,7 +43,7 @@ export async function init(options: {
     !options.skipInstall &&
     !(packageJson.dependencies?.bakery || packageJson.devDependencies?.bakery)
   ) {
-    await $`bun add --dev git+https://github.com/luketurner/bakery.git`;
+    await $`bun add --dev @luketurner/bakery`;
   }
 
   if (!existsSync(".github/workflows/publish.yml")) {
